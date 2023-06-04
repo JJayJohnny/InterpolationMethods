7cb5ca932bd38db997dc234534a17d4ac42a949e
JJayJohnny <jasiek.barczewski@gmail.com>
2023-06-04 18:54:52 +0200

    added spline method

--- main.py
+++ main.py
@@ -21,15 +21,20 @@ def fi(i, x, interpolationNodes):
 def lagrangeMethod(x, y, interpolationNodesCount):
     interpolationNodes = []
     interpolationNodesValues = []
-    step = int(len(x)/interpolationNodesCount)
-    for i in range(0, len(x), step):
-        interpolationNodes.append(x[i])
-        interpolationNodesValues.append(y[i])
+    # step = int(len(x)/interpolationNodesCount)
+    # # wyznaczenie rownooddalonych wezlow interpolacji
+    # for i in range(0, len(x), step):
+    #     interpolationNodes.append(x[i])
+    #     interpolationNodesValues.append(y[i])
+    idx = np.round(np.linspace(0, len(x) - 1, interpolationNodesCount)).astype(int)
+    interpolationNodes = x[idx]
+    interpolationNodesValues = y[idx]
 
     yInter = []
     xInter = []
 
     # for xI in np.linspace(0, x[len(x)-1], 1000):
+    # interpolujemy w tych samych punktach co znane wartosci aby moc porownac jakosc interpolacji
     for xI in x:
         fx = 0
         for i in range(0, len(interpolationNodes)):
@@ -37,8 +42,86 @@ def lagrangeMethod(x, y, interpolationNodesCount):
         yInter.append(fx)
         xInter.append(xI)
 
-    return xInter, yInter
+    return np.array(xInter), np.array(yInter)
 
+
+def splineMethod(x, y, interpolationNodesCount):
+    interpolationNodes = []
+    interpolationNodesValues = []
+    # step = int(x.shape[0] / interpolationNodesCount)
+    # # wyznaczenie rownooddalonych wezlow interpolacji
+    # for i in range(0, len(x), step):
+    #     interpolationNodes.append(x[i])
+    #     interpolationNodesValues.append(y[i])
+    idx = np.round(np.linspace(0, len(x) - 1, interpolationNodesCount)).astype(int)
+    interpolationNodes = x[idx]
+    interpolationNodesValues = y[idx]
+
+    A = np.zeros((4*(interpolationNodesCount-1), 4*(interpolationNodesCount-1)))
+    Y = np.zeros((4*(interpolationNodesCount-1), 1))
+
+    row = 0
+    # wpisanie wartosci na poczatkach przedzialow
+    for i in range(0, interpolationNodesCount-1):
+        A[row, 4*i] = 1
+        Y[row] = interpolationNodesValues[i]
+        row += 1
+
+    # wpisanie wartosci na koncach przedzialow
+    for i in range(1, interpolationNodesCount):
+        h = interpolationNodes[i] - interpolationNodes[i-1]
+        A[row, (i-1)*4 + 0] = 1
+        A[row, (i-1)*4 + 1] = h
+        A[row, (i-1)*4 + 2] = pow(h, 2)
+        A[row, (i-1)*4 + 3] = pow(h, 3)
+        Y[row] = interpolationNodesValues[i]
+        row += 1
+
+    # pierwsze pochodne
+    for i in range(1, interpolationNodesCount-1):
+        h = interpolationNodes[i] - interpolationNodes[i - 1]
+        A[row, (i-1)*4 + 1] = 1
+        A[row, (i-1)*4 + 2] = 2 * h
+        A[row, (i-1)*4 + 3] = 3 * pow(h, 2)
+        A[row, i*4 + 1] = -1
+        Y[row] = 0
+        row += 1
+
+    # drugie pochodne
+    for i in range(1, interpolationNodesCount-1):
+        h = interpolationNodes[i] - interpolationNodes[i - 1]
+        A[row, (i-1)*4 + 2] = 2
+        A[row, (i-1)*4 + 3] = 6 * h
+        A[row, i*4 + 2] = -2
+        Y[row] = 0
+        row += 1
+
+    # zerowanie drugich pochodnych na poczatku i koncu
+    A[row, 2] = 1
+    Y[row] = 0
+    row += 1
+    h = interpolationNodes[len(interpolationNodes) - 1] - interpolationNodes[len(interpolationNodes) - 2]
+    A[row, A.shape[1]-2] = 2
+    A[row, A.shape[1]-1] = 6 * h
+    Y[row] = 0
+
+    parameters = np.linalg.solve(A, Y)
+    print(np.allclose(np.dot(A, parameters), Y))
+    split = -1
+    yInter = []
+    xInter = []
+    for xI in x:
+        if xI in interpolationNodes:
+            split += 1
+            if split >= interpolationNodesCount - 1:
+                split -= 1
+        h = xI - interpolationNodes[split]
+        fx = parameters[split*4, 0] + parameters[split*4+1, 0]*h + parameters[split*4+2, 0]*pow(h, 2) + parameters[split*4+3, 0]*pow(h, 3)
+        xInter.append(xI)
+        yInter.append(fx)
+
+
+    return np.array(xInter), np.array(yInter)
 
 
 
@@ -48,9 +131,11 @@ if __name__ == '__main__':
 
     x = data['x'].to_numpy()
     y = data['y'].to_numpy()
+    # x = np.array([1, 3, 5])
+    # y = np.array([6, -2, 4])
 
-    xInter, yInter = lagrangeMethod(x, y, 15)
+    xInter, yInter = lagrangeMethod(x, y, 40)
 
     plt.plot(x, y)
-    plt.plot(xInter, yInter)
+    plt.plot(xInter[100:-100], yInter[100:-100])
     plt.show()
